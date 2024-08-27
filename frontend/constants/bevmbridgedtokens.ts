4b8ff8b1c73f12a0151e779263a1742902ffc285
kamalbuilds <geniusamansingh@gmail.com>
2024-08-27 17:26:18 +0530

    bevm bridged tokens

---
+++ frontend/constants/bevmbridgedtokens.ts
@@ -0,0 +1,163 @@
+export const bevmbridgedtokens =  [
+    {
+      "chainId": 11501,
+      "address": "0x88290434e8F5a2Fd3Dc3ef31A1bEF19d74372C24",
+      "type": "BRC20" ,
+      "name": "Bridged BRC20 SATS",
+      "symbol": "SATS",
+      "decimals": 18,
+      "logoURI": "https://raw.githubusercontent.com/btclayer2/bridge-tokenlist/main/asset/sats.png"
+    },
+    {
+      "chainId": 11501,
+      "address": "0xe3D7fd661Eb7A141fC485E4f2679995e4fEf0298",
+      "type": "BRC20" ,
+      "name": "Bridged BRC20 ORDI",
+      "symbol": "ORDI",
+      "decimals": 18,
+      "logoURI": "https://raw.githubusercontent.com/btclayer2/bridge-tokenlist/main/asset/ordi.png"
+    },
+    {
+      "chainId": 11501,
+      "address": "0x0AE14249b5CD93d63BE2F0394A377F244fB06f9D",
+      "type": "BRC20" ,
+      "name": "Bridged BRC20 RATS",
+      "symbol": "RATS",
+      "decimals": 18,
+      "logoURI": "https://raw.githubusercontent.com/btclayer2/bridge-tokenlist/main/asset/rats.png"
+    },
+    {
+      "chainId": 11501,
+      "address": "0x50D38DB58f494ded068e4B0c051449fa012e5BB4",
+      "type": "BRC20" ,
+      "name": "Bridged BRC20 BTCS",
+      "symbol": "BTCS",
+      "decimals": 18,
+      "logoURI": "https://raw.githubusercontent.com/btclayer2/bridge-tokenlist/main/asset/btcs.png"
+    },
+    {
+      "chainId": 11501,
+      "address": "0x00D13C732e73Da7D1376f84cd2D0FeF9E792b4e7",
+      "type": "BRC20" ,
+      "name": "Bridged BRC20 AINN",
+      "symbol": "AINN",
+      "decimals": 18,
+      "logoURI": "https://raw.githubusercontent.com/btclayer2/bridge-tokenlist/main/asset/ainn.png"
+    },
+    {
+      "chainId": 11501,
+      "address": "0x041bbB9c16fDBa8C805565D0bB34931e37895EC9",
+      "type": "RUNES" ,
+      "name": "ORDINALS•RUNES",
+      "symbol": "ORDINALS•RU",
+      "decimals": 0,
+      "logoURI": ""
+    },
+    {
+      "chainId": 11501,
+      "address": "0x8d6F6210EdA57cE98f8293cEbCfe0f730e142895",
+      "type": "RUNES" ,
+      "name": "Bridged Runes DOG•GO•TO•THE•MOON",
+      "symbol": "🐕",
+      "decimals": 5,
+      "logoURI": "https://raw.githubusercontent.com/btclayer2/bridge-tokenlist/main/asset/dm.png"
+    },
+    {
+      "chainId": 11501,
+      "address": "0xdab0d843d8e6369930920d6356E7FB16Bf54f611",
+      "type": "RUNES" ,
+      "name": "Bridged Runes UNCOMMON•GOODS",
+      "symbol": "⧉",
+      "decimals": 0,
+      "logoURI": ""
+    },
+    {
+      "chainId": 11501,
+      "address": "0x2A9AF616b6D57d50Dd93A410FA64055EE03198d7",
+      "type": "RUNES" ,
+      "name": "Bridged Runes SATOSHI•NAKAMOTO",
+      "symbol": "丰",
+      "decimals": 8,
+      "logoURI": "https://raw.githubusercontent.com/btclayer2/bridge-tokenlist/main/asset/sn.png"
+    },
+    {
+      "chainId": 11501,
+      "address": "0x915247bf09471922e2c6da6f69fc9114708e8a26",
+      "type": "ERC20" ,
+      "name": "Bridged USD Coin",
+      "symbol": "USDC",
+      "decimals": 6,
+      "logoURI": "https://raw.githubusercontent.com/btclayer2/bridge-tokenlist/main/asset/usdc.png"
+    },
+    {
+      "chainId": 11501,
+      "address": "0xa67ed736649f2958a35fd249a584151056b4b745",
+      "type": "ERC20" ,
+      "name": "Bridged Tether USD",
+      "symbol": "USDT",
+      "decimals": 6,
+      "logoURI": "https://raw.githubusercontent.com/btclayer2/bridge-tokenlist/main/asset/usdt.svg"
+    },
+    {
+      "chainId": 11501,
+      "address": "0x948def74953a18ebd854a5b015f63b0910be58cc",
+      "type": "ERC20" ,
+      "name": "Bridged Ethereum Token",
+      "symbol": "ETH",
+      "decimals": 18,
+      "logoURI": "https://raw.githubusercontent.com/btclayer2/bridge-tokenlist/main/asset/eth.svg"
+    },
+    {
+      "chainId": 11501,
+      "address": "0x172828C5936c6C765C22bF3CE9C4f94e7420E640",
+      "type": "ERC20" ,
+      "name": "Bridged Curve.Fi USD Stablecoin",
+      "symbol": "crvUSD",
+      "decimals": 18,
+      "logoURI": "https://raw.githubusercontent.com/btclayer2/bridge-tokenlist/main/asset/crvusd.png"
+    },
+    {
+      "chainId": 11501,
+      "address": "0x21247221d69De837BB7E31bf448B9F97D10a36c0",
+      "type": "ERC20" ,
+      "name": "Bridged StakeStone Ether",
+      "symbol": "STONE",
+      "decimals": 18,
+      "logoURI": "https://raw.githubusercontent.com/btclayer2/bridge-tokenlist/main/asset/stone.jpg"
+    },
+    {
+      "chainId": 1,
+      "address": "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+      "type": "ERC20" ,
+      "name": "Ethereum USD Coin",
+      "symbol": "USDC",
+      "decimals": 6,
+      "logoURI": "https://raw.githubusercontent.com/btclayer2/bridge-tokenlist/main/asset/usdc.png"
+    },
+    {
+      "chainId": 1,
+      "address": "0xdac17f958d2ee523a2206206994597c13d831ec7",
+      "type": "ERC20" ,
+      "name": "Ethereum Tether USD",
+      "symbol": "USDT",
+      "decimals": 6,
+      "logoURI": "https://raw.githubusercontent.com/btclayer2/bridge-tokenlist/main/asset/usdt.svg"
+    }, {
+      "chainId": 1,
+      "address": "0xf939e0a03fb07f59a73314e73794be0e57ac1b4e",
+      "type": "ERC20" ,
+      "name": "Curve.Fi USD Stablecoin",
+      "symbol": "crvUSD",
+      "decimals": 18,
+      "logoURI": "https://raw.githubusercontent.com/btclayer2/bridge-tokenlist/main/asset/crvusd.png"
+    },
+    {
+      "chainId": 1,
+      "address": "0x7122985656e38BDC0302Db86685bb972b145bD3C",
+      "type": "ERC20" ,
+      "name": "StakeStone Ether",
+      "symbol": "STONE",
+      "decimals": 18,
+      "logoURI": "https://raw.githubusercontent.com/btclayer2/bridge-tokenlist/main/asset/stone.jpg"
+    }
+]
