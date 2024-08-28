d901c4de225f457e2b9f56000afd36a852a586d7
kamalbuilds <geniusamansingh@gmail.com>
2024-08-29 03:16:11 +0530

    landing updated

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -13,10 +13,10 @@ export default function Component() {
               <div className="flex flex-col justify-center space-y-4">
                 <div className="space-y-2">
                   <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none">
-                    Unlock the Power of Investing with BEVm
+                    Unlock the Power of CrossChain Bitcoin Investing with PWR
                   </h1>
                   <p className="max-w-[600px] text-muted-foreground md:text-xl">
-                    Seamlessly invest across multiple blockchains with our advanced investment management platform.
+                    Seamlessly invest across multiple BitcoinL2s with our advanced investment management platform.
                   </p>
                   <div className="flex flex-col gap-2 min-[400px]:flex-row">
                     <Link
